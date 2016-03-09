e1a4cebdecf61d113df2871a2e4384ce50b18eb6
Suellen.Karoline.Heidorn <suellen.heidorn@senior.com.br>
2016-03-09 13:51:01 -0300

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name previsaoTempoFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the previsaoTempoFrontendApp
+ */
+angular.module('previsaoTempoFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
