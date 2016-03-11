108a9b5f6ba6aadb0a324e07f96a9d2b66af92d0
Suellen.Karoline.Heidorn <suellen.heidorn@senior.com.br>
2016-03-11 15:11:49 -0300

    started page construction

--- app/app.js
+++ app/app.js
@@ -2,6 +2,18 @@ angular.module('myApp',[])
 .controller('WeatherCtrl', function(){
     var vm = this;
 
-    vm.teste = 'aosidoasjduio';
+    //vm.teste = 'aosidoasjduio';
+
+    Highcharts.chart('chart', {
+
+				    xAxis: {
+				        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+				            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+				    },
+
+				    series: [{
+				        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+				    }]
+				});
 
 });
